8742f23176eed61ef37cb097be0ee6d74a3ba539
Mario Mitchell <mario@undergroundfx.com>
2018-10-25 04:09:53 -0700

    VARIABLES AND DATA TYPES

---
+++ Program2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+
+    class Program
+    {
+        static void Main()
+        {
+        Console.WriteLine("The Tech Academy");
+        Console.WriteLine("Student Daily Report");
+
+        Console.WriteLine("What course are you on?");
+        string course = Console.ReadLine();
+
+        Console.WriteLine("What page number?");
+        string page = Console.ReadLine();
+        int pageNumber = Convert.ToInt16(page);
+
+        Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
+        string help = Console.ReadLine();
+        bool anyHelp = Convert.ToBoolean(help);
+
+        Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
+        string experiences = Console.ReadLine();
+
+        Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
+        string feedback = Console.ReadLine();
+
+        Console.WriteLine("How many hours did you study today?");
+        string hours = Console.ReadLine();
+        sbyte study = Convert.ToSByte(hours);
+    }
+}
+
+
