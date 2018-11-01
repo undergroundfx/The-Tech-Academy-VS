1aabe4056d78b48f0d2849219a5f37e7dd7e8aae
Mario Mitchell <mario@undergroundfx.com>
2018-10-31 20:44:50 -0700

    BOOLEAN COMPARISON

---
+++ Program7.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Boolean Comparison
+
+            bool on = true;
+
+            while (on == true)
+            {
+                Console.WriteLine("Hello!");
+                on = false;
+            }
+
+            do
+            {
+                Console.WriteLine("Hi!");
+                on = true;
+            }
+
+            while (on == false);
+
+
+
+            Console.Read();
+        }
+
+
+
+    }
+}
